c28d9ced97f0964fb850f847a77401c1b35ab0a0
Wookjin Jang <jin090130@webcash.co.kr>
2023-08-09 17:22:54 +0900

    create

---
+++ app/src/main/java/com/wookgenie/android_design_pattern_mvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wookgenie.android_design_pattern_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
